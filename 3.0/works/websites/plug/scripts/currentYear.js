1eb0a4cfedf56e8a12922618c85ff3632c6b66b0
arosamiran <arosamiran@outlook.ph>
2017-08-21 11:16:18 +0800

    removed my previous profile

--- 3.0/works/websites/plug/scripts/currentYear.js
+++
@@ -1,8 +0,0 @@
-window.onload = init;
-
-function init() {
-	// Get year
-	var yearHandler = document.getElementById('year');
-	var currentYear = new Date().getFullYear();
-	yearHandler.innerHTML = "&copy; " + currentYear;
-}
