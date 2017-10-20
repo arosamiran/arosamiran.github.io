739fc1252878173292c9714fb0b11118c0bc920e
arosamiran <arosamiran@outlook.ph>
2017-10-20 19:13:45 +0800

    removed the "burger"  icon

---
+++ js/script7.js
@@ -0,0 +1,7 @@
+let intro = document.getElementById('intro');
+
+intro.style.height = intro.clientHeight + "px";
+intro.style.width = intro.clientWidth + "px";
+intro.style.backgroundImage = "url(../img/coffee-beans-d.png)";
+intro.style.backgroundColor = "#f9a4a4";
+console.log(intro);
