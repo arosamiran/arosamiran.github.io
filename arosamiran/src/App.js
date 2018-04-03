c509c7f6fc60577d19aa75e644558735b272cd3e
arosamiran <arosamiran@gmail.com>
2018-04-03 19:46:02 +0800

    ib=ntroduction, bg

--- arosamiran/src/App.js
+++ arosamiran/src/App.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './doodlr.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <div className="header">
+          <div>
+            <img src={logo} className="logo" alt="Doodlr logo" />            
+          </div>
+        </div>
+          <div className="intro">
+            <p>
+              Hey there! What's up? I'm Angelo Rosamiran, fan of web development and everything open source. I'm also interested in designing
+              and photography.
+            </p>
+
+            <p>
+              Through self-studying, I've learned how to program with JavaScript. Currently, I'm updating what I know about the language
+            (It's a never-ending process) and VR experiences for the web (with A-Frame!).
+            </p>
+            {/* <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Farosamiran.me&width=25px&layout=button_count&action=like&show_faces=true&share=false&height=21&appId"
+                width="75px" height="21" style="border:none;overflow:hidden;" scrolling="no" frameborder="0" allowTransparency="true"></iframe> */}
+          </div>
+
+      <div className="links">
+        <h1>Links</h1>
+          <div>
+          <a target="_blank" href="www.facebook.com/arosamiran">Facebook</a>
+          </div>
+          <div>
+          <a target="_blank" href="www.twitter.com/arosamiran">Twitter</a>
+          </div>
+          <div>
+          <a target="_blank" href="http://instagram.com/arosamiran">Instagram</a>
+          </div>
+          <div>
+          <a target="_blank" href="https://linkedin.com/in/arosamiran">Linkedin</a>
+          </div>
+          <div>
+          <a target="_blank" href="https://plus.google.com/+AngeloRosamiran">Google+</a>
+          </div>
+          </div>
+        </div>
     );
   }
 }
-
 export default App;
